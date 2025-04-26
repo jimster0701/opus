585bf7186e35cfb609eb8db2b1a52f02c4f4da65
James P-M <s5416624@bournemouth.ac.uk>
2025-04-26 23:02:01 +0100

    Remove getAllPostComments query from commentRouter to streamline comment handling

--- src/server/api/routers/comment.ts
+++ src/server/api/routers/comment.ts
@@ -7,22 +7,6 @@ import {
 } from "~/server/api/trpc";
 
 export const commentRouter = createTRPCRouter({
-  getAllPostComments: protectedProcedure
-    .input(
-      z.object({
-        postId: z.number(),
-      })
-    )
-    .query(async ({ ctx, input }) => {
-      const comments = await ctx.db.comment.findMany({
-        orderBy: { createdAt: "desc" },
-        where: { postId: input.postId },
-        include: { createdBy: true },
-      });
-
-      return comments ?? null;
-    }),
-
   createComment: protectedProcedure
     .input(
       z.object({
