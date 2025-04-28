5e0a6dba10e04ddd8fbf8273c453da08f4c90a83
James P-M <jimmypm0701@gmail.com>
2025-04-28 22:09:59 +0100

    feat: refactor Taskbox component to manage interests state and improve rendering of interest display

--- src/app/_components/tasks/taskbox.tsx
+++ src/app/_components/tasks/taskbox.tsx
@@ -1,8 +1,12 @@
 "use client";
-import { type Task } from "~/types/task";
 import styles from "../../index.module.css";
+import { type Task } from "~/types/task";
+import { type Interest } from "~/types/interest";
 import { type User } from "~/types/user";
 import { trpc } from "~/utils/trpc";
+import { defaultInterests } from "~/const/defaultVar";
+import { useEffect, useState } from "react";
+import { shuffle } from "../util";
 
 interface TaskboxProps {
   task: Task;
@@ -11,9 +15,24 @@ interface TaskboxProps {
 }
 
 export default function Taskbox(props: TaskboxProps) {
-  const interests = trpc.interest.getInterestsById.useQuery({
+  const [interests, setInterests] = useState<Interest[]>(
+    shuffle(
+      defaultInterests.filter((i) => props.task.interestIds.includes(i.id))
+    )
+  );
+  const interestResult = trpc.interest.getInterestsById.useQuery({
     interestIds: props.task.interestIds,
   });
+  useEffect(() => {
+    if (interestResult.isLoading) {
+      return;
+    }
+    if (interestResult.data?.length != 0) {
+      setInterests((prev) =>
+        shuffle([...prev, ...(interestResult.data as Interest[])])
+      );
+    }
+  }, [interestResult.isLoading, interestResult.data]);
   return (
     <div key={props.task.id} className={styles.taskContainer}>
       <div className={styles.taskIconContainer}>
@@ -22,14 +41,24 @@ export default function Taskbox(props: TaskboxProps) {
       <div className={styles.taskContentContainer}>
         <p className={styles.taskTitle}>{props.task.name}</p>
         <p className={styles.taskText}>{props.task.description}</p>
-        <p className={styles.taskInterests}>
-          Based on:{" "}
-          {interests.isLoading ? (
-            <>Loading...</>
-          ) : (
-            interests.data?.map((interest) => interest.name).join(", ")
-          )}
-        </p>
+        <div className={styles.flexRow}>
+          <p className={styles.taskInterests}>Based on: </p>
+          {interests.map((interest) => (
+            <div
+              key={interest.id}
+              style={{
+                border: `${interest.colour} 1px solid`,
+                ["--text-glow" as any]: `linear-gradient(to top left,rgb(70, 70, 70), ${interest.colour})`,
+              }}
+              className={styles.glowingNugget}
+            >
+              <p className={styles.glowingNuggetText}>
+                {interest.icon}
+                {interest.name}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
