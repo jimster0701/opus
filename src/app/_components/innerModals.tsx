571e6ea68bfaacd357a05f2babc60e6b3b8dfdc7
James P-M <jimmypm0701@gmail.com>
2025-05-06 21:05:10 +0100

    fix: correct typos and improve clarity in About Us modal content

--- src/app/_components/innerModals.tsx
+++ src/app/_components/innerModals.tsx
@@ -4,22 +4,22 @@ export function AboutUsInnerModal() {
       <h1>About Opus</h1>
       <br />
       <h4>
-        Opus was created as the main artefact for dissertation.
+        Opus was created as the main artefact for my dissertation.
         <br />
         <br />
         {
-          "The original idea was to create an app that used AI to generate tasks based off of the user's interests."
+          "The original idea was to create an app that used AI to generate tasks based on the user's interests."
         }
         <br />
         <br />
-        This concept evolved into the currnet Opus, a social media platform
+        This concept evolved into the current Opus, a social media platform
         centred around creating tasks, and sharing hobbies and interests.
         <br />
         <br />
         {
-          "If you need help, click the title 'Opus' in the navbar when you enter or"
+          "If you need help, click the title 'Opus' in the navbar when you enter or reach out to"
         }
-        reach out to
+        <br />
         <a href={"mailto:jimmypm0701@gmail.com"}>jimmypm0701@gmail.com</a>
       </h4>
     </>
